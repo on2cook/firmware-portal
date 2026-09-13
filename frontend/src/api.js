cccb1b038be9cfcd305528e7f8869e23bbe0e278
Niravon2cook <nirav@on2cook.com>
2026-09-13 21:43:54 +0530

    change exe download to apk download

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,4 @@
 const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:4000/api';
-console.log("API_BASE =", API_BASE);
 function getToken() {
   return localStorage.getItem('fp_token');
 }
